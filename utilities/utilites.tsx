a8511576e61ae8ed02c10a514b77e2fc144b15e8
Ilyas Mohamed <zswaleh@ymail.com>
2024-02-28 15:35:10 -0600

    added code for drawing an object onto a canvas

--- utilities/utilites.tsx
+++ utilities/utilites.tsx
@@ -3,3 +3,25 @@ export const clearBoard = (context: CanvasRenderingContext2D | null) => {
         context.clearRect(0, 0, 1000, 600);
     }
 }
+
+export interface IObjectBody {
+    x: number;
+    y: number;
+  }
+  
+  export const drawObject = (
+    context: CanvasRenderingContext2D | null,
+    objectBody: IObjectBody[],
+    fillColor: string,
+    strokeStyle = "#146356"
+  ) => {
+    if (context) {
+      objectBody.forEach((object: IObjectBody) => {
+        context.fillStyle = fillColor;
+        context.strokeStyle = strokeStyle;
+        context?.fillRect(object.x, object.y, 20, 20);
+        context?.strokeRect(object.x, object.y, 20, 20);
+      });
+    }
+  };
+  
