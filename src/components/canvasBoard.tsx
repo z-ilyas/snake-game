a2a441611b7f42aaaab10141962dc1f51eb00ad5
Ilyas Mohamed <zswaleh@ymail.com>
2024-02-28 15:14:13 -0600

    able to store canvas contex in a state variable

--- src/components/canvasBoard.tsx
+++ src/components/canvasBoard.tsx
@@ -1,18 +1,28 @@
+import { useEffect, useRef, useState } from 'react';
+
 export interface ICanvasBoard {
-    height: number;
-    width: number;
-  }
-  
-const canvasBoard = ({ height, width }: ICanvasBoard) => {
-    return (
-      <canvas
-        style={{
-          border: "3px solid black",
-        }}
-        height={height}
-        width={width}
-      />
-    );
+  height: number;
+  width: number;
+}
+
+const CanvasBoard = ({ height, width }: ICanvasBoard) => {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const [context, setContext] = useState<CanvasRenderingContext2D | null>(null);
+
+  useEffect(() => {
+    setContext(canvasRef.current && canvasRef.current.getContext('2d'));
+  }, [canvasRef.current]);
+
+  return (
+    <canvas
+      ref={canvasRef}
+      style={{
+        border: "3px solid black",
+      }}
+      height={height}
+      width={width}
+    />
+  );
 };
 
-export default canvasBoard;
+export default CanvasBoard;
