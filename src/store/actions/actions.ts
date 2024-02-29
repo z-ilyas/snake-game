fb445a906641d0f7e7a0f586daed1d327b692361
Ilyas Mohamed <zswaleh@ymail.com>
2024-02-29 17:52:24 -0600

    added directions in which snake moves

---
+++ src/store/actions/actions.ts
@@ -0,0 +1,26 @@
+export const MOVE_RIGHT = "MOVE_RIGHT";
+export const MOVE_LEFT = "MOVE_LEFT";
+export const MOVE_UP = "MOVE_UP";
+export const MOVE_DOWN = "MOVE_DOWN";
+
+export const RIGHT = "RIGHT";
+export const LEFT = "LEFT";
+export const UP = "UP";
+export const DOWN = "DOWN";
+
+export const SET_DIS_DIRECTION = "SET_DIS_DIRECTION";
+
+export interface ISnakeCoord {
+  x: number;
+  y: number;
+}
+export const makeMove = (dx: number, dy: number, move: string) => ({
+  type: move,
+  payload: [dx, dy]
+});
+
+export const setDisDirection = (direction: string) => ({
+  type: SET_DIS_DIRECTION,
+  payload: direction
+});
+
--- src/store/reducers/globalStateReducer.ts
+++ src/store/reducers/globalStateReducer.ts
@@ -19,3 +19,4 @@ export const globalState: IGlobalState = {
   ],
     disallowedDirection: ""
 };
+
